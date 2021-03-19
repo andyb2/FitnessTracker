022adf9a78a3fdc80f30258ab6154c36fb47f273
AMB <a.michael.boyle@gmail.com>
2021-03-19 15:32:27 -0400

    mongo connection update

--- server.js
+++ server.js
@@ -10,10 +10,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/Fitness", {
-    useNewUrlParser: true,
-    useFindAndModify: false
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/Fitness',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
 
 app.get('/exercise', (req, res) => {
     res.redirect('/exercise.html')
